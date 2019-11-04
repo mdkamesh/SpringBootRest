4ee8a8c641083f2a4b5f80a6ab8c6e0c5dff1868
mrao48 <mrao48@IN5CG4360L97.CSCMWS.CSCMWS.COM>
2019-11-04 11:15:15 +0530

    Spring Boot Initial Commits for Spring Boot Rest Examples

---
+++ src/main/java/com/kamesh/springbootlearnings/SpringRest/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.kamesh.springbootlearnings.SpringRest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "HelloWorld";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World Bean!!");
+	}
+	// /hello-world/pathvariable/kamesh
+	@GetMapping(path="/hello-world/pathvariable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+
+}
---
+++ src/test/java/com/kamesh/springbootlearnings/SpringRest/SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamesh.springbootlearnings.SpringRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
